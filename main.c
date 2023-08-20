78d0e652322bb454729fb2dfb142b14c4c433a8f
ikrayemlik <133769214+ikrayemlik@users.noreply.github.com>
2023-08-20 12:08:07 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Main fonksiyonu içerisinde verilen sayýnýn basamaklarýný toplamýný rekürsif bir fonksiyon kullanarak
+bulunuz.*/
+
+
+
+int topla(int);
+
+int main(){
+	int sayi;
+	printf("Sayinizi giriniz : "); scanf("%d",&sayi);
+	
+	printf("\nBasamaklarin toplami ==> %d",topla(sayi));
+	
+	return 0;
+}
+
+int topla(int x){
+	if(x==0)
+		return 0;
+		
+	else
+		return x%10 + topla(x/10);	
+}
